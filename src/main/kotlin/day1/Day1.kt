3a5372101c7981e8b6a648bb97849274cdba8416
beltram.maldant <beltram.maldant@soprabanking.com>
2021-12-01 16:07:29 +0100

    feat: simpler day1

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -3,19 +3,9 @@ package day1
 import readInput
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        var count = 0
-        input.map { it.toInt() }.reduce { a, b -> if (a < b) count++; b }
-        return count
-    }
+    fun part1(input: List<String>) = input.map { it.toInt() }.windowed(2).count { (a, b) -> a < b }
 
-    fun part2(input: List<String>): Int {
-        var count = 0
-        val nums = input.map { it.toInt() }
-        val sums = nums.mapIndexed { idx, i -> i + nums.getOrElse(idx + 1) { 0 } + nums.getOrElse(idx + 2) { 0 } }
-        sums.reduce { a, b -> if (a < b) count++; b }
-        return count
-    }
+    fun part2(input: List<String>) = input.map { it.toInt() }.windowed(3).map { (a, b, c) -> a + b + c }.windowed(2).count { (a, b) -> a < b }
 
     check(part1(readInput(1, "-part1-test")) == 7)
     println(part1(readInput(1)))
