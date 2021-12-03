1b60416d32725a64c523fb2f7bc01a4f85102c50
beltram.maldant <beltram.maldant@soprabanking.com>
2021-12-03 14:11:44 +0100

    day3 part 2

--- src/main/kotlin/day3/Day3.kt
+++ src/main/kotlin/day3/Day3.kt
@@ -5,21 +5,46 @@ import kotlin.math.absoluteValue
 
 fun main() {
     fun List<String>.part1(len: Int): Long {
-        val middle = size / 2
-        val gamma = map { it.toCharArray().map { c -> c.digitToInt() }.toIntArray() }
+        return map { it.toCharArray().map { c -> c.digitToInt() }.toIntArray() }
             .fold(IntArray(len)) { acc, it -> acc.merge(it) }
-            .map { it.cmp(middle) }
+            .map { it.cmp(size / 2) }
             .joinToString("")
-        val epsilon = gamma.inv()
-        return gamma.toLong(2) * epsilon.toLong(2)
+            .computeResult()
+    }
+
+    fun List<List<Int>>.oxygen(len: Int, idx: Int = 0): List<Int> = when {
+        size > 1 -> when {
+            count { it[idx] == 1 }.toFloat() >= middle() -> filter { it[idx] == 1 }
+            else -> filter { it[idx] == 0 }
+        }.oxygen(len, idx + 1)
+        else -> flatten()
+    }
+
+    fun List<List<Int>>.co2(len: Int, idx: Int = 0): List<Int> = when {
+        size > 1 -> {
+            when {
+                count { it[idx] == 0 }.toFloat() <= middle() -> filter { it[idx] == 0 }
+                else -> filter { it[idx] == 1 }
+            }.co2(len, idx + 1)
+        }
+        else -> first()
+    }
+
+    fun List<String>.part2(len: Int): Long {
+        return sorted()
+            .map { it.toCharArray().map { c -> c.digitToInt(2) } }
+            .run { oxygen(len).toLong() * co2(len).toLong() }
     }
 
     check(readInput(3, "-part1-test").part1(5) == 198L)
-    println(readInput(3).part1(12))
-//    check(readInput(3, "-part1-test").part2() == 900)
-//    println(readInput(3).part2())
+    check(readInput(3).part1(12) == 2972336L)
+    check(readInput(3, "-part1-test").part2(5) == 230L)
+    check(readInput(3).part2(12) == 3368358L)
 }
 
+private fun String.computeResult(): Long = let { gamma -> gamma.toLong(2) * gamma.inv().toLong(2) }
 private fun IntArray.merge(other: IntArray) = zip(other) { a, b -> a + b }.toIntArray()
-private fun Int.cmp(other: Int) = if (this > other) 1 else 0
+private fun Int.cmp(other: Int) = if (this >= other) 1 else 0
 private fun String.inv() = toCharArray().map { it.digitToInt(2) }.map { (it - 1).absoluteValue }.joinToString("")
+private fun List<*>.middle() = (size.div(2.0)).toFloat()
+private fun List<Int>.toLong() = joinToString("").toLong(2)
