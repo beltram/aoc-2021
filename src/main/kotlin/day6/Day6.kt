dc47c806ef1b7eb7295831a657cc1e4f9c38cfd9
beltram.maldant <beltram.maldant@soprabanking.com>
2021-12-06 17:40:22 +0100

    day 6 part 2

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -4,32 +4,23 @@ import readInput
 
 fun main() {
 
-    fun List<Fish>.elapseTime(days: Int): Int {
-        return if (days == 0) {
-            count()
-        } else {
-            flatMap { it.elapse() }.elapseTime(days - 1)
+    fun MutableList<Long>.elapseTime(days: Int): Long = when (days) {
+        0 -> sum().toLong()
+        else -> {
+            val fishesAtZero = first()
+            drop(1).toMutableList()
+                .also { it[6] = it[6] + fishesAtZero; it.add(fishesAtZero) }
+                .elapseTime(days - 1)
         }
     }
 
-    fun String.part1(days: Int): Int {
-        return split(',').map { Fish.from(it) }.elapseTime(days)
-    }
+    fun String.toAges() = split(',').map { it.toInt() }
+        .let { ages -> MutableList(9) { index -> ages.count { it == index }.toLong() } }
+        .toMutableList()
 
-    check(readInput(6, "-test").first().part1(18) == 26)
-    check(readInput(6, "-test").first().part1(80) == 5934)
-    println(readInput(6).first().part1(80))
-}
-
-internal data class Fish(val ttl: Int) {
-    companion object {
-        fun from(raw: String) = Fish(raw.toInt())
-        fun reset() = Fish(6)
-        fun newBaby() = Fish(8)
-    }
-
-    fun elapse(): List<Fish> = when (ttl) {
-        0 -> listOf(reset(), newBaby())
-        else -> listOf(Fish(ttl - 1))
-    }
+    check(readInput(6, "-test").first().toAges().elapseTime(18) == 26L)
+    check(readInput(6, "-test").first().toAges().elapseTime(80) == 5934L)
+    check(readInput(6).first().toAges().elapseTime(80) == 391671L)
+    check(readInput(6, "-test").first().toAges().elapseTime(256) == 26984457539L)
+    check(readInput(6).first().toAges().elapseTime(256) == 1754000560399L)
 }
